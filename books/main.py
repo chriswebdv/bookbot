a4f068b51fc80e00b5a182842524433f35ac6498
chriswebdv <chriswebdv@gmail.com>
2024-03-25 16:53:01 -0600

    a description of your changes here

--- books/main.py
+++ books/main.py
@@ -3,7 +3,18 @@ def main():
     text = get_book_text(book_path)
     num_words = count_words(text)
     chars_dict = get_chars_dict(text)
-    print(chars_dict)
+    chars_sorted_list = chars_dict_to_sorted_list(chars_dict)
+
+    print(f"--- Begin report of {book_path} ---")
+    print(f"{num_words} words found in the document")
+    print()
+
+    for item in chars_sorted_list:
+        if not item["char"].isalpha():
+            continue
+        print(f"The '{item['char']}' character was found {item['num']} times")
+
+    print("--- End report ---")
 
 def get_book_text(path):
     with open(path) as f:
@@ -23,6 +34,17 @@ def get_chars_dict(text):
 def count_words(text):
     words = len(text.split())
     return words
+
+def sort_on(d):
+    return d["num"]
+
+
+def chars_dict_to_sorted_list(num_chars_dict):
+    sorted_list = []
+    for ch in num_chars_dict:
+        sorted_list.append({"char": ch, "num": num_chars_dict[ch]})
+    sorted_list.sort(reverse=True, key=sort_on)
+    return sorted_list
 
 main()
 
