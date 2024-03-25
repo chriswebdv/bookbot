b5e72bcad7454c273d4f81149bab1ad75b9c68d1
chriswebdv <chriswebdv@gmail.com>
2024-03-25 16:44:51 -0600

    update code

--- books/main.py
+++ books/main.py
@@ -2,17 +2,27 @@ def main():
     book_path = ".gitignore/frankenstein.txt"
     text = get_book_text(book_path)
     num_words = count_words(text)
-    print(f"{num_words} words found in the document")
-
+    chars_dict = get_chars_dict(text)
+    print(chars_dict)
 
 def get_book_text(path):
     with open(path) as f:
         return f.read()
 
+def get_chars_dict(text):
+    chars = {}
+    for c in text:
+        lowered = c.lower()
+        if lowered in chars:
+            chars[lowered] += 1
+        else:
+            chars[lowered] = 1
+    return chars
+
+
 def count_words(text):
     words = len(text.split())
     return words
-
 
 main()
 
